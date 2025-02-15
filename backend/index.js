77e8cce4898c546d92a540456c5049465b025db3
Navish <navishgoyal50@gmail.com>
2025-02-15 15:30:34 +0530

    cors changes

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,7 @@ const router = require('./routes/index');
 const {app,server} = require('./socket/index');
 
 app.use(express.json());
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}));
+app.use(cors());
 app.use(cookieParser());
 app.use('/api',router);
 
